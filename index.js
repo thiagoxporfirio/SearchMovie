6c81f5cebebf9e012ce06e811782379b8aac15f1
Thiago Porfirio | Zanini Renk <thiago.porfirio@zaninirenk.com.br>
2023-05-11 09:57:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let movieNameRef = document.getElementById("movie-name")
+let searchBtn = document.getElementById("search-btn")
+let result = document.getElementById("result")
+
+let getMovie = () => {
+    let movieName = movieNameRef.value
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey${key}`
+
+
+
+    if(movieName.length <= 0 ){
+        result.innerHTML = `<h3 class="msg">Por favor coloque o nome do filme<h3>`
+    }
+    else{
+        fetch(url)
+            .then((resp) => {
+                resp.json()
+            }).then((data) => {
+
+                if(data.response == "true"){
+                    result.innerHTML = `
+                    
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star-icon.svg">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                    `
+                }
+
+                else{
+                    result.innerHTML = `
+                        <h3 class="msg" > Ocorreu um erro :( <h3>
+                    `
+                }
+            })
+    }
+}
+
+movieNameRef.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      searchBtn.click();
+    }
+
+
+});
+
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
